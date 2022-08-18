1f3c004f2b4aed6910f1216ca6b990dd7eeb7eca
luchoxx87 <ale.capo.9634@gmail.com>
2022-08-18 15:41:48 -0300

    45u890k049504

--- Scripts/01 SP.sql
+++ Scripts/01 SP.sql
@@ -1,14 +1,17 @@
+-- Active: 1632321143175@@127.0.0.1@3306@mvcrestaurante
 
 USE MVCRestaurante;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS altaRestaurante $$
 CREATE PROCEDURE altaRestaurante (out unidRestaurante INT, unNombre VARCHAR(45), unaDireccion VARCHAR(45), unMail VARCHAR(45), unTelefono INT, unaContrasenia CHAR(64))
 BEGIN
       INSERT INTO Restaurante (nombre, direccion, mail, telefono, contrasenia)
-                   VALUES (unNombre, unaDireccion, unMail, unTelefono, unaContrasenia);
+                   VALUES (unNombre, unaDireccion, unMail, unTelefono, sha2(unaContrasenia, 256));
       SET unidRestaurante =  LAST_INSERT_ID();
 END $$
 
 DELIMITER $$
+DROP PROCEDURE IF EXISTS altaCategoria $$
 CREATE PROCEDURE altaCategoria(out unidCategoria INT, unNombre VARCHAR(45))
 BEGIN
       INSERT INTO Categoria (nombre)
@@ -17,6 +20,7 @@ BEGIN
 END $$
 
 DELIMITER $$
+DROP PROCEDURE IF EXISTS altaPlato $$
 CREATE PROCEDURE altaPlato(out unidPlato INT, unidRestaurante INT, unidCategoria INT, unNombre VARCHAR(45), unPrecio DECIMAL (7,2))
 BEGIN
 	 INSERT INTO Plato (idRestaurante, idCatergoria, nombre, precio)
--- Scripts/Pruebas.sql
+++ Scripts/Pruebas.sql
@@ -1,3 +1,7 @@
+-- Active: 1632321143175@@127.0.0.1@3306@mvcrestaurante
 USE MVCRestaurante;
 
 call altaCategoria(@idPollo, 'Pollo');
+call altaCategoria(@idHamburguesa, 'Hambuerguesa');
+call altaRestaurante(@idRestaurante,'el loro burger', 'av.libertador270','loroburger@gmail.com', 1123456789,'1234');
+call altaplato(@idplato, @idCategoria, @idRestaurante,'Hamburguesa triple con salsa chedar',12000.99);
