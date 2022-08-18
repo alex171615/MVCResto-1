d158f8d29db38a93680ed39af739cbe3375e5c87
Alexis <ale.capo.9634@gmail.com>
2022-08-18 17:51:01 -0300

    jjkjj

--- Scripts/Pruebas.sql
+++ Scripts/Pruebas.sql
@@ -6,4 +6,4 @@ call altaCategoria(@idHamburguesa, 'Hamburguesa');
 call altaCategoria(@idSandwich, 'SandWich');
 call altaRestaurante(@idLoroburguer,'el loro burger', 'av.libertador270','loroburger@gmail.com', 1123456789,'1234');
 call altaRestaurante(@idLoroSandWich,'el loro Sandwich', 'av.libertador270','loroSandwich@gmail.com', 1123456789,'1234');
-call altaPlato(@idPlato,1,1,'Hamburguesa triple con salsa chedar', 12.90);
+call altaPlato(@idPlato,1,1,'Hamburguesa triple con salsa chedar', 92.9);
