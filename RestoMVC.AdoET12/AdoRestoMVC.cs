4fdb4398fc39344f06d24fda19b781e6518d6bc7
Noguera Alexis Kurt <ale.capo.9634@gmail.com>
2022-09-22 16:04:03 -0300

    hhhh

---
+++ RestoMVC.AdoET12/AdoRestoMVC.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using RestoMVC.Core.AdoET12.Mapeadores;
+using RestoMVC.AdoET12;
+using RestoMVC.Core;
+using  et12.edu.ar.AGBD.Ado;
+
+namespace RestoMVC.AdoET12
+{
+    public class AdoRestoMVC : IAdo 
+    {
+        
+    }
+}
