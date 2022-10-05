1aacc0e87e6bb7075915ff7b32f132d5296a2f89
AlexisNogueraKurt <ale.capo.9634@gmail.com>
2022-10-05 13:37:12 -0300

    cambios

--- RestoMVC.Core/Restaurante.cs
+++ RestoMVC.Core/Restaurante.cs
@@ -20,15 +20,16 @@ namespace RestoMVC.Core
 
         public string Contrasenia {get; set;}
         public List<Plato> Platos {get; set;}
-        public Restaurante () {
-            Platos = new List<Plato>();
-        }
-
+        public Restaurante () => Platos = new List<Plato>();
         public Restaurante(string nombre, string direccion, string mail, int telefono, string contrasenia) : this ()
         {
             Nombre = nombre;
-            Direccion = direccion
+            Direccion = direccion;
+            Mail = mail;
+            Telefono = telefono;
+            Contrasenia = contrasenia;
         }
+        public void AgregarPlato(Plato plato) => Platos.Add(plato);
         
         
     }
