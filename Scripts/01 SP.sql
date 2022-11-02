a1eaa1e04c1693facabd7b806284e704cb1df350
AlexisNogueraKurt <ale.capo.9634@gmail.com>
2022-11-02 15:30:01 -0300

    elim
    This reverts commit 2bad7fb3666b09160e31e5c423cf59b2c8ed80ea.

--- Scripts/01 SP.sql
+++ Scripts/01 SP.sql
@@ -104,8 +104,7 @@ DROP PROCEDURE
     IF EXISTS eliminarRestaurante $$
 CREATE PROCEDURE
     EliminarRestaurante(out unidRestaurante SMALLINT) BEGIN
-DELETE *
-FROM Restaurante
+DELETE FROM Restaurante
 WHERE
     idRestaurante = unidRestaurante;
 
