b20ad7a096b2b23e74db2dc0b84d9db10000599b
AlexisNogueraKurt <ale.capo.9634@gmail.com>
2022-11-07 15:20:00 -0300

    delete trigger

--- Scripts/02 Trigger.sql
+++
@@ -1,8 +0,0 @@
-DELIMITER $$
-DROP TRIGGER IF EXISTS befdeletePlato $$
-CREATE TRIGGER befdeletePlato BEFORE DELETE ON plato
-FOR EACH ROW
-BEGIN
-    DELETE FROM Restaurante
-    WHERE idPlato = OLD.idPlato;
-END $$
