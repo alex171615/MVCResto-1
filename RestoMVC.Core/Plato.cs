b29b64965b69ea41d93bd60c6a13007e17adbe70
AlexisNogueraKurt <ale.capo.9634@gmail.com>
2022-11-17 15:29:12 -0300

    REDME

--- RestoMVC.Core/Plato.cs
+++ RestoMVC.Core/Plato.cs
@@ -22,6 +22,17 @@ namespace RestoMVC.Core
 
         public Plato (int id ) => Id = id;
         public Plato (string nombre) => Nombre = nombre;
+        public List<Plato> Platos { get; set; }
+        public Plato (string nombre, string direccion, string mail, int telefono, string contrasenia) : this()
+        
+    {
+        Id = Id;
+        categoria = categoria;
+        restaurante = restaurante;
+        Nombre = nombre;
+        Precio = Precio;
+    }
+    public void AgregarPlato(Plato plato) => Platos.Add(plato);
 
         
     }
